005c59e626f306af0ee68a84a0029a376dc796f7
Razib Hasan <razib.hasan@tuni.fi>
2024-01-26 13:10:02 +0200

    basic/011: add Javadoc to class

---
+++ baisc/011/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * @author      Razib Hasan <razib.hasan@tuni.fi>
+ * @version     2024.0126 (last modified)
+ * @since       17.0 (miminimum Java version)
+ */
+class HelloWorld {
+
+   
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
