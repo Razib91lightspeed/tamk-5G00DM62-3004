7c8f5de2e5ba9a3d4455b9c4d714507c9ec725cb
Razib Hasan <razib.hasan@tuni.fi>
2024-02-08 04:22:48 +0200

    014

---
+++ basic/014/Pythagoras.java
@@ -0,0 +1,32 @@
+/**
+ * The Pythagoras class calculates the hypotenuse of a right triangle using the Pythagorean theorem.
+ * It provides a method to calculate the hypotenuse given the lengths of the other two sides.
+ *
+ * @author Razib Hasan <razib.hasan@tuni.fi>
+ * @version 2024.0208 (last modified)
+ * @since 17.0 (minimum Java version)
+ */
+public class Pythagoras {
+    /**
+     * Exercise: 1.4
+     * Write the Pythagoras program.
+     *
+     * @param a The length of one side of the right triangle.
+     * @param b The length of the other side of the right triangle.
+     * @return The length of the hypotenuse of the right triangle.
+     */
+    public static double calculate(double a, double b){
+        double c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+        return c;
+    }
+
+    /**
+     * Main method to demonstrate the use of the calculate method.
+     *
+     * @param args Command line arguments. Not used.
+     */
+    public static void main(String[] args) {
+        double answer = calculate(3, 4);
+        System.out.println(answer);
+    }
+}
